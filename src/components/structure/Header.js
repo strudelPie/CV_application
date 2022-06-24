8923fbd92b6cdc7df6d778a5cf6cacacd11c5eb5
strudelPie <edward.strudwick1@gmail.com>
2022-06-24 20:49:20 +0100

    Third Commit

--- src/components/structure/Header.js
+++ src/components/structure/Header.js
@@ -2,21 +2,18 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const HeaderContainer = styled.div`
-    background-color: #333;
+    background-color: #282c34;
 `
 const HeaderWrapper = styled.div`
-    font-family: 'Nunito', cursive;
     display: flex;
     align-items: center;
-    max-width: 1800px;
-    max-height: 100px;
-    padding: 3rem 8rem;
-    margin: 0 auto;
     color: #eee;
-    font-size: 2.2rem;
+    font-size: 1.5rem;
+    margin-left: 100px;
     @media (max-width: 1600px) {
         justify-content: center;
-    }
+        margin-left: 0;
+    }    
 `
 
 class Header extends Component {
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    background-color: #E0E0E0;
+  }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
